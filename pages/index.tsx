263b3f93c61fe5bb8dd395129d4ffb7f9d80e1a3
Joe Prisk <joe.prisk@thinkmill.com.au>
2023-03-14 10:23:16 +1300

    added favicon and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,19 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Index() {
-  return <Link href="/keystatic">Go to Keystatic</Link>;
+  return (
+    <>
+      <Head>
+        <title>Keystatic | Blank Template</title>
+        <meta
+          name="description"
+          content="Create a new project using keystatic"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Link href="/keystatic">Go to Keystatic</Link>
+    </>
+  );
 }
